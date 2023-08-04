1246dca3403ee6024c4ed21ada46558cf89b1048
Federico Baldini <federico.baldini7@gmail.com>
2023-08-04 15:41:15 +0200

    Update tooltip.ts

--- src/components/tooltip.ts
+++ src/components/tooltip.ts
@@ -1,17 +1,41 @@
 /**
- * Represents a custom tooltip element that displays additional information when hovered over.
+ * Represents a custom tooltip element that displays additional information when hovered.
  */
 class Tooltip extends HTMLElement {
-  private tooltipIcon: HTMLElement | null;
-  private tooltipContainer: HTMLElement | null;
+  private iconElement: HTMLElement | null;
+  private containerElement: HTMLElement | null;
+  private text: string;
 
   /**
    * Constructor for creating a new Tooltip instance.
    */
   public constructor() {
     super();
-    this.tooltipIcon = null;
-    this.tooltipContainer = null;
+    this.iconElement = null;
+    this.containerElement = null;
+    this.text = "";
+
+    // Create and attach a shadow DOM to the custom tooltip element.
+    this.attachShadow({ mode: "open" });
+
+    if (this.shadowRoot) {
+      // Set the initial shadow DOM content with inline styles and a slot for the outside content.
+      this.shadowRoot.innerHTML = `
+        <style>
+          span {
+            position: relative;
+          }
+          div {
+            background: #000000;
+            color: #ffffff;
+            position: absolute;
+            z-index: 10;
+          }
+        </style>
+        <slot>default</span>
+        <span>(?)</span>
+      `;
+    }
   }
 
   /**
@@ -19,49 +43,58 @@ class Tooltip extends HTMLElement {
    */
   private showTooltipHandler(): void {
     // Create a <div> element to serve as the tooltip container.
-    this.tooltipContainer = document.createElement("div");
+    this.containerElement = document.createElement("div");
 
     // Set the text content of the tooltip container to display the tooltip message.
-    this.tooltipContainer.textContent = "This is the tooltip text!";
+    this.containerElement.textContent = this.text;
 
-    // Append the tooltip container to the custom tooltip element.
-    this.appendChild(this.tooltipContainer);
+    if (this.shadowRoot) {
+      // Append the tooltip container to the custom tooltip element's shadow DOM.
+      this.shadowRoot.appendChild(this.containerElement);
+    }
   }
 
   /**
    * Event handler to hide the tooltip.
    */
   private hideTooltipHandler(): void {
-    // Check if tooltipContainer exists before removing it from the DOM.
-    if (this.tooltipContainer) {
-      // Remove the tooltip container from the custom tooltip element.
-      this.removeChild(this.tooltipContainer as Node);
+    if (this.containerElement && this.shadowRoot) {
+      // Remove the tooltip container from the custom tooltip element's shadow DOM.
+      this.shadowRoot.removeChild(this.containerElement as Node);
     }
   }
 
   /**
    * Called when the element is inserted into the DOM.
    * It creates a tooltip icon and appends it to the element.
+   * Retrieves and stores the "text" attribute if present.
    */
   public connectedCallback(): void {
-    // Create a <span> element to serve as the tooltip icon.
-    this.tooltipIcon = document.createElement("span");
+    // Check if the custom tooltip element has the "text" attribute.
+    if (this.hasAttribute("text")) {
+      // Get the tooltip text from the "text" attribute and store it in the "text" property.
+      this.text = this.getAttribute("text") as string;
+    }
 
-    // Set the text content of the tooltip icon to "(?)".
-    this.tooltipIcon.textContent = "(?)";
+    if (this.shadowRoot) {
+      // Get the reference to the tooltip icon (span) within the custom tooltip element's shadow DOM.
+      this.iconElement = this.shadowRoot.querySelector("span");
 
-    // Add event listeners to show/hide tooltip on mouseenter/mouseleave.
-    this.tooltipIcon.addEventListener(
-      "mouseenter",
-      this.showTooltipHandler.bind(this)
-    );
-    this.tooltipIcon.addEventListener(
-      "mouseleave",
-      this.hideTooltipHandler.bind(this)
-    );
+      if (this.iconElement) {
+        // Add event listeners to show/hide tooltip on mouseenter/mouseleave.
+        this.iconElement.addEventListener(
+          "mouseenter",
+          this.showTooltipHandler.bind(this)
+        );
+        this.iconElement.addEventListener(
+          "mouseleave",
+          this.hideTooltipHandler.bind(this)
+        );
 
-    // Append the tooltip icon to the custom tooltip element.
-    this.appendChild(this.tooltipIcon);
+        // Append the tooltip icon to the custom tooltip element's shadow DOM.
+        this.shadowRoot.appendChild(this.iconElement);
+      }
+    }
   }
 
   /**
@@ -69,14 +102,14 @@ class Tooltip extends HTMLElement {
    * It removes the event listeners to avoid potential memory leaks.
    */
   public disconnectedCallback(): void {
-    // Check if tooltipIcon exists before removing event listeners.
-    if (this.tooltipIcon) {
+    // Check if iconElement exists before removing event listeners.
+    if (this.iconElement) {
       // Remove event listeners for mouseenter and mouseleave.
-      this.tooltipIcon.removeEventListener(
+      this.iconElement.removeEventListener(
         "mouseenter",
         this.showTooltipHandler.bind(this)
       );
-      this.tooltipIcon.removeEventListener(
+      this.iconElement.removeEventListener(
         "mouseleave",
         this.hideTooltipHandler.bind(this)
       );
