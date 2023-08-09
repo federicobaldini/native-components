ab3cfe98887f82725699414ccb6440b3a2e73227
Federico Baldini <federico.baldini7@gmail.com>
2023-08-09 10:28:22 +0200

    Update modal.ts

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -1,9 +1,23 @@
+/**
+ * Represents a custom modal element that displays a dialog box for confirmation.
+ */
 class Modal extends HTMLElement {
+  private backdropElement: HTMLElement | null;
+  private modalElement: HTMLElement | null;
+  private cancelButton: HTMLButtonElement | null;
+  private confirmButton: HTMLButtonElement | null;
+  private isOpen: boolean;
+
   /**
    * Constructor for creating a new Modal instance.
    */
   public constructor() {
     super();
+    this.backdropElement = null;
+    this.modalElement = null;
+    this.cancelButton = null;
+    this.confirmButton = null;
+    this.isOpen = false;
 
     // Create and attach a shadow DOM to the custom modal element.
     this.attachShadow({ mode: "open" });
@@ -20,6 +34,8 @@ class Modal extends HTMLElement {
             height: 100vh;
             background: rgba(0, 0, 0, 0.5);
             z-index: 2;
+            opacity: 0;
+            pointer-events: none;
           }
 
           #modal {
@@ -36,6 +52,8 @@ class Modal extends HTMLElement {
             flex-direction: column;
             justify-content: space-between;
             overflow-y: auto;
+            opacity: 0;
+            pointer-events: none;
           }
 
           header {
@@ -71,11 +89,114 @@ class Modal extends HTMLElement {
             <slot></slot>
           </section>
           <section id="actions">
-            <button>Cancel</button>
-            <button>Confirm</button>
+            <button id="cancel-button">Cancel</button>
+            <button id="confirm-button">Confirm</button>
           </section>
         </div>
       `;
+    }
+  }
+
+  /**
+   * Event handler to show the modal.
+   */
+  private showModalHandler(): void {
+    if (this.backdropElement && this.modalElement) {
+      this.isOpen = true;
+
+      // Update styles to make the modal and backdrop visible and interactive.
+      this.backdropElement.style.opacity = "1";
+      this.backdropElement.style.pointerEvents = "all";
+      this.modalElement.style.opacity = "1";
+      this.modalElement.style.pointerEvents = "all";
+    }
+  }
+
+  /**
+   * Event handler to hide the modal.
+   */
+  private hideModalHandler(): void {
+    if (this.backdropElement && this.modalElement) {
+      this.isOpen = false;
+
+      // Update styles to hide the modal and backdrop.
+      this.backdropElement.style.opacity = "0";
+      this.backdropElement.style.pointerEvents = "none";
+      this.modalElement.style.opacity = "0";
+      this.modalElement.style.pointerEvents = "none";
+    }
+  }
+
+  /**
+   * Called when the element is inserted into the DOM.
+   * Sets up event listeners for buttons and initializes element references.
+   */
+  public connectedCallback(): void {
+    if (this.shadowRoot) {
+      // Get references to elements within the shadow DOM.
+      this.backdropElement = this.shadowRoot.querySelector("#backdrop");
+      this.modalElement = this.shadowRoot.querySelector("#modal");
+      this.cancelButton = this.shadowRoot.querySelector("#cancel-button");
+      this.confirmButton = this.shadowRoot.querySelector("#confirm-button");
+    }
+
+    // Add event listeners for buttons to hide the modal.
+    if (this.cancelButton) {
+      this.cancelButton.addEventListener(
+        "click",
+        this.hideModalHandler.bind(this)
+      );
+    }
+
+    if (this.confirmButton) {
+      this.confirmButton.addEventListener(
+        "click",
+        this.hideModalHandler.bind(this)
+      );
+    }
+  }
+
+  /**
+   * Called when the element is removed from the DOM.
+   * Removes event listeners to prevent memory leaks.
+   */
+  public disconnectedCallback(): void {
+    if (this.cancelButton) {
+      this.cancelButton.removeEventListener("click", this.hideModalHandler);
+    }
+
+    if (this.confirmButton) {
+      this.confirmButton.removeEventListener("click", this.hideModalHandler);
+    }
+  }
+
+  /**
+   * Specifies the attributes to be observed for changes.
+   * In this case, it observes changes to the "open" attribute.
+   */
+  public static get observedAttributes(): Array<string> {
+    return ["open"];
+  }
+
+  /**
+   * Called when the observed attributes change.
+   * Updates the modal visibility based on the "open" attribute value.
+   */
+  public attributeChangedCallback(
+    name: string,
+    oldValue: any,
+    newValue: any
+  ): void {
+    switch (name) {
+      case "open":
+        if (newValue) {
+          this.showModalHandler();
+        } else {
+          this.hideModalHandler();
+        }
+        break;
+      default:
+        break;
     }
   }
 }
